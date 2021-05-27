d46987f0fea5bd8921e4c4a1f3568bdaab817f6d
denver1981denver <84741346+denver1981denver@users.noreply.github.com>
2021-05-27 22:01:07 +0300

    Add files via upload

---
+++ lesson01/script.js
@@ -0,0 +1,12 @@
+"use strict";
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+// создание переменных
+alert("Hello World");
+// Вывод сообщения на экран в модальном окне
+console.log("На вашем устройстве обнаружено вредоносное ПО");
+// Вывод сообщения в консоль
